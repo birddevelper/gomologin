16d8a4dc1b9d746c32944f621dd0886442e49e30
bird <mostafa_shaeri_tj@yahoo.com>
2022-04-27 01:46:43 +0430

    Basic functionality implemented

---
+++ auth.go
@@ -0,0 +1,115 @@
+package gologin
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+
+	"github.com/gorilla/securecookie"
+)
+
+var LoginPage string = "./templates/login.html"
+var SessionExpireTimeout int = 120 // in second
+var LoginPath string = "/login"
+
+// cookie handling
+var Logintemplates = template.Must(template.ParseFiles(
+	LoginPage,
+))
+
+var cookieHandler = securecookie.New(
+	securecookie.GenerateRandomKey(64),
+	securecookie.GenerateRandomKey(32))
+
+func GetUserName(request *http.Request) (userName string) {
+	if cookie, err := request.Cookie("session"); err == nil {
+		cookieValue := make(map[string]string)
+		if err = cookieHandler.Decode("session", cookie.Value, &cookieValue); err == nil {
+			userName = cookieValue["name"]
+		}
+	}
+	return userName
+}
+
+func setSession(userName string, response http.ResponseWriter) {
+	value := map[string]string{
+		"name": userName,
+	}
+	if encoded, err := cookieHandler.Encode("session", value); err == nil {
+		cookie := &http.Cookie{
+			Name:   "session",
+			Value:  encoded,
+			Path:   "/",
+			MaxAge: SessionExpireTimeout,
+		}
+		http.SetCookie(response, cookie)
+	}
+}
+
+func clearSession(response http.ResponseWriter) {
+	cookie := &http.Cookie{
+		Name:   "session",
+		Value:  "",
+		Path:   "/",
+		MaxAge: -1,
+	}
+	http.SetCookie(response, cookie)
+}
+
+// login handler
+
+func doLogin(response http.ResponseWriter, request *http.Request) {
+	name := request.FormValue("username")
+	pass := request.FormValue("password")
+	redirectTarget := LoginPath + "?wrong"
+	if name != "" && pass != "" {
+
+		if name == "user" && pass == "1234" {
+			setSession(name, response)
+			redirectTarget = "/"
+		}
+	}
+	http.Redirect(response, request, redirectTarget, 302)
+}
+
+func loginView(w http.ResponseWriter, r *http.Request) {
+
+	err := Logintemplates.ExecuteTemplate(w, "login", nil)
+
+	if err != nil {
+		http.Error(w, fmt.Sprintf("login: couldn't parse template: %v", err), http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+}
+
+func LoginHandler() http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		if r.Method == "GET" {
+			loginView(w, r)
+		} else if r.Method == "POST" {
+			doLogin(w, r)
+		}
+	})
+}
+
+// logout handler
+func LogoutHandler() http.Handler {
+	return http.HandlerFunc(func(response http.ResponseWriter, request *http.Request) {
+		clearSession(response)
+		http.Redirect(response, request, "/", 302)
+	})
+}
+
+func LoginRequired(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		userName := GetUserName(r)
+		if userName != "" {
+			next.ServeHTTP(w, r)
+		} else {
+			http.Redirect(w, r, LoginPath+"?expired", 302)
+		}
+
+	})
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/birddevelper/gologin
+
+go 1.16
+
+require github.com/gorilla/securecookie v1.1.1
